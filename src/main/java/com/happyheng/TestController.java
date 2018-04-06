a09aedf9e29be1188886941a1e8bd9c456422ff3
happyheng <happyheng9522@163.com>
2018-04-06 21:32:29 +0800

    增加redis环境

--- src/main/java/com/happyheng/TestController.java
+++ src/main/java/com/happyheng/TestController.java
@@ -1,7 +1,9 @@
 package com.happyheng;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import redis.clients.jedis.Jedis;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -12,10 +14,26 @@ import javax.servlet.http.HttpServletRequest;
 @RestController
 public class TestController {
 
+    @Autowired
+    private Jedis jedis;
 
     @RequestMapping("/test")
     public String testRequest(HttpServletRequest httpServletRequest) {
         return "TestController";
+    }
+
+    @RequestMapping("/testRedis")
+    public String testRedis(HttpServletRequest httpServletRequest) {
+        return "Server is running: " + jedis.ping();
+    }
+
+
+    public static void main(String[] args) {
+        // 连接本地的 Redis 服务
+        Jedis jedis = new Jedis("localhost");
+        System.out.println("Connection to server sucessfully");
+        // 查看服务是否运行
+        System.out.println("Server is running: " + jedis.ping());
     }
 
 }
